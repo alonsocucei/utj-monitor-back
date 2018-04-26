db82a7c40e4e519af432aff48d62b2e225856d0d
Alonso Sanchez <alonso.sanchez@wizeline.com>
2018-04-26 01:07:36 -0500

    Copy script

---
+++ src/java/utils/copy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ $# -lt 1 ] 
+then
+   echo "usage: copy.sh Source.java, you have to be at src/java"
+   echo " where Source.java is the assumed name of the Java source file"
+   exit 1
+fi
+
+javac -cp ../../../tomee/lib/*:. -d ../../../tomee/webapps/utj#api $1
+if [ $? -ne 0 ]
+then
+  echo "Error during compilation"  
+fi
