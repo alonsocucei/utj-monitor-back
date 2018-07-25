4972d84c9e17c3e7efef5b0335da8eb0860f9834
Alonso Sanchez <alonso.sanchez@wizeline.com>
2018-07-24 19:59:50 -0500

    Modify compile.sh

--- src/java/utils/compile.sh
+++ src/java/utils/compile.sh
@@ -2,12 +2,8 @@
 
 find ../ -name "*.java" > ../sources.txt
 
-javac -cp ../../../../tomee/lib/*:. -d ../../../../tomee/webapps/utj#api/WEB-INF/classes @sources.txt
+javac -cp ../../../../tomee/lib/*:. -d ../../../../tomee/webapps/utj#api2/WEB-INF/classes @sources.txt
 if [ $? -ne 0 ]
 then
   echo "Error during compilation"  
 fi
-
-cd ../../../../tomee/bin
-shutdown.sh
-startup.sh
